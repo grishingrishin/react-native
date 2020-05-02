6be65c0861de07b000d3b311e1c7538f8577c617
ygorkagrishin <grishingrishin@yahoo.com>
2020-05-02 11:35:42 +0300

    Добавлен styled-components

--- components/Auth.js
+++ components/Auth.js
@@ -1,75 +1,70 @@
 import React, { Component } from 'react';
-import { 
-  StyleSheet, 
-  View,
-  ImageBackground,
-  Text,
-  TextInput,
-  TouchableOpacity
-} from 'react-native';
+import styled from 'styled-components';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#693fa9',
-  },
-  backgroundImage: {
-    flex: 1,
-    paddingTop: 32,
-    paddingBottom: 32,
-    justifyContent: 'space-between',
-    resizeMode: 'cover',
-  },
-  header: {
-    position: 'relative',
-    top: '15%',
-  },
-  title: {
-    fontFamily: 'Roboto-light',
-    textAlign: 'center',
-    fontSize: 42,
-    color: '#fff',
-  },
-  auth: {
-    paddingRight: 16,
-    paddingLeft: 16,
-  },
-  textInput: {
-    paddingTop: 16,
-    paddingRight: 14,
-    paddingBottom: 16,
-    paddingLeft: 14,
-    borderBottomWidth: 1,
-    borderBottomColor: '#d3c8e6'
-  },
-  forgot: {
-    alignItems: 'flex-end',
-  },
-  forgorText: {
-    textTransform: 'uppercase',
-    paddingTop: 14,
-    paddingBottom: 14,
-    fontSize: 12,
-    color: '#fff',
-  },
-  submit: {
-    alignItems: 'center',
-    height: 54,
-    backgroundColor: '#eb2054',
-  },
-  submitText: {
-    textTransform: 'uppercase',
-    fontSize: 18,
-    lineHeight: 54,
-    color: '#fff',
-  },
-  small: {
-    marginTop: 14,
-    textAlign: 'center',
-    fontSize: 10,
-    color: '#fff'
-  },
-});
+const Wrapper = styled.View`
+  flex: 1;
+  background-color: #693fa9;
+`;
+
+const Container = styled.ImageBackground`
+  flex: 1;
+  padding-top: 32px;
+  padding-bottom: 32px;
+  justify-content: space-between;
+  resize-mode: cover;
+`;
+
+const Header = styled.View`
+  position: relative;
+  top: 15%;
+`;
+
+const Title = styled.Text`
+  text-align: center;
+  font-size: 42px;
+  color: #fff;
+`;
+
+const WrapperInput = styled.View`
+  padding-right: 16px;
+  padding-left: 16px;
+`;
+
+const TextInput = styled.TextInput`
+  padding-top: 16px;
+  padding-right: 14px;
+  padding-bottom: 16px;
+  padding-left: 14px;
+  border-bottom-width: 1px;
+  border-bottom-color: #d3c8e6;
+`;
+
+const Forgot = styled.TouchableOpacity`
+  align-items: flex-end;
+`;
+
+const ForgotText = styled.Text`
+  text-transform: uppercase;
+  padding-top: 14px;
+  padding-bottom: 14px;
+  font-size: 12px;
+  color: #fff;
+`;
+
+const Action = styled.View``;
+
+const Submit = styled.TouchableOpacity`
+  align-items: center;
+  height: 54px;
+  background-color: #eb2054;
+`;
+
+const SubmitText = styled.Text`
+  text-transform: uppercase;
+  font-size: 18px;
+  line-height: 54px;
+  color: #fff;
+`;
 
 class Auth extends Component {
   constructor() {
@@ -101,59 +96,40 @@ class Auth extends Component {
     });
   }
 
-  goToForgotPassword = () => {
+  goToForgotPassword = () => {}
 
-  }
-
-  loginHandler = () => {
-
-  }
+  loginHandler = () => {}
 
   render() {
     return (
-      <View style={styles.container}>
-        <ImageBackground source={require('../assets/images/auth-bg.jpg')} style={styles.backgroundImage}>
-          <View style={styles.header}>
-            <Text 
-              style={styles.title} 
-              accessibilityRole='header'
-            >Welcome!</Text>
-          </View>
-          <View style={styles.auth}>
-            <TextInput 
-              style={{ 
-                ...styles.textInput, 
-                marginBottom: 32 
-              }}
+      <Wrapper>
+        <Container source={require('../assets/images/auth-bg.jpg')}>
+          <Header>
+            <Title accessibilityRole='header'>Welcome!</Title>
+          </Header>
+          <WrapperInput>
+            <TextInput
               onChangeText={text => this.changeLoginHandler(text)}
               value={this.state.data.login}
               placeholder='Login'
             />
             <TextInput 
-              style={styles.textInput}
               onChangeText={text => this.changePasswordHandler(text)}
               value={this.state.data.password}
               secureTextEntry={true}
               placeholder='Password'
             />
-            <TouchableOpacity
-              style={styles.forgot}
-              onPress={this.goToForgotPassword}
-            >
-              <Text style={styles.forgorText}>Forgot Password?</Text>
-            </TouchableOpacity>
-          </View>
-          <View>
-            <TouchableOpacity
-              style={styles.submit}
-              activeOpacity={0.9}
-              onPress={this.loginHandler}
-            >
-              <Text style={styles.submitText}>Login</Text>
-            </TouchableOpacity>
-          </View>
-        </ImageBackground>
-      </View>
+            <Forgot onPress={this.goToForgotPassword}>
+              <ForgotText>Forgot Password?</ForgotText>
+            </Forgot>
+          </WrapperInput>
+          <Action>
+            <Submit activeOpacity={0.9} onPress={this.loginHandler}>
+              <SubmitText>Login</SubmitText>
+            </Submit>
+          </Action>
+        </Container>
+      </Wrapper>
     );
   }
 }
